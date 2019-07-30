e8cedf3e7343b436131423d84456a388119cba28
mahdi darzi <mahdidarzi1024@gmail.com>
2019-07-30 16:18:10 +0430

    i made muti files for test

---
+++ src/assets/styles/header.js
@@ -0,0 +1,56 @@
+import {StyleSheet}  from 'react-native';
+export default  headerStyles = StyleSheet.create({
+    headersbackground : {
+        backgroundColor : '#34495e'
+    },
+    headerLeftStyle1 : {
+        marginRight : 10,
+        marginLeft : 5
+    },
+    headerLeftStyle2 : {
+        marginRight : 30,
+        marginLeft : -60
+    },
+    headerLeftStyle3 : {
+        marginLeft : 5,
+        marginRight : -20
+    },
+    drawerStlye : {
+        color : 'white' ,
+        fontSize : 35
+
+    },
+    body : {
+        marginRight : 0,
+        marginLeft : -10
+    },
+    bodyText : {
+        color : 'white',
+        fontSize : 22
+    },
+    personIconView : {
+        backgroundColor : '#607D8B',
+        width : 40,
+        height : 40,
+        borderRadius : 20,
+        marginTop : 3,
+        alignItems : 'center'
+    },
+    iconPerson : {
+        marginTop : 5,
+        color : '#00BCD4'
+    }
+
+
+
+
+
+  
+
+
+
+
+ 
+
+
+})
